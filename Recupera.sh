d206e43ec23aac3da02ded4dcdfce7a0680e37e3
theluismen <luismaocampo28@gmail.com>
2024-03-05 12:59:26 +0100

    Script 2 Recupera hecho

--- Recupera.sh
+++ Recupera.sh
@@ -2,9 +2,10 @@
 
 function usage () {
     echo "Ús Dolent:"
-    echo "  $0 [-D] fitxer.tgz file.txt [ files.txt ... ] directory"
+    echo "  $0 [-D] fitxer.tgz file.txt [ files.txt ... ] directori"
 }
 
+# Variables d'opcions
 D_MODE=0
 
 # Comprobar que el nombre d'args es apropiat 
@@ -35,7 +36,7 @@ gz=${@:files_bgn:1}
 files=${@:files_bgn+1:$#-files_bgn-1}
 dir=${@:$#}
 
-# Comprobar que el fitxer comprimir existeix 
+# Comprobar que existe el comprimido
 if [[ ! -e $gz ]]; then
     echo "No s'ha trobat el fitxer: $gz"
     exit 1
@@ -43,15 +44,28 @@ fi
 
 # Comprobar que el directori existeix i sino crearlo
 if [[ ! -d $dir ]]; then
-    mkdir $dir
-    echo creat $dir
+    echo "El directorio destino $dir no existe"
+    if [[ $D_MODE -eq 1 ]]; then
+        echo "[-D] - Se crearia el directorio destino: $dir"
+    else 
+        mkdir $dir
+        echo "Directorio destino $dir creado"
+    fi
 fi
 
+# Para cada archivo, compruebo si esta, si existe en el directorio destino i si es asi lo etiqueto con la fecha
 for file in $files; do 
+    # Si el fitxer es troba al comprimit
     if ! tar --list -zf $gz $file &> /dev/null; then
-        echo $file not exists in $gz
+        echo "No existeix $file a $gz"
     else 
-        tar -zxf $gz $file
-        mv $file $dir        
+        [[ $D_MODE -eq 1 ]] && echo "[-D] - Se extraeria $file de $gz" || tar -zxf $gz $file
+
+        if [[ -e "$dir/$file" ]]; then
+            filefecha="$file.$(date +%Y.%m.%d:%H.%M.%S)"
+            [[ $D_MODE -eq 1 ]] && echo "[-D] - Fichero repetido. $file -> $filefecha"
+            file=$filefecha
+        fi
+        [[ $D_MODE -eq 1 ]] && echo "[-D] - Se moveria $file a $dir" || mv $file $dir
     fi
 done
