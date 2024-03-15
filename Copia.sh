d172c44fa620f292ed648b8931253be64d4436ea
theluismen <luismaocampo28@gmail.com>
2024-03-15 16:15:02 +0100

    Script 1 Copia hecho

--- Copia.sh
+++ Copia.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Funció per mostrar l'ús del script
 function usage () {
     echo "Ús Dolent:"
@@ -31,48 +30,50 @@ files=${@:files_bgn+1:$#-files_bgn-1}
 
 # Comprobar si el comprimido existe
 if [[ ! -e $gz ]]; then
-    if [[ $D -eq 1 ]]; then
+    if [[ $D -eq 1 ]]; then # Se tendría que crear
         echo "[-D] - Se crearia el archivo comprimido: $gz."
-    else 
-        tar zcf $gz $files
+    else
+        for file in ${files[@]}; do [[ ! -e $file ]] && echo "$file no existe. Abortando..." && exit 1; done
+        tar zcf $gz $files  # Se crea
     fi
-    exit 1
+    exit 0
 fi
 
-# # Itera pels fitxers i directoris d'entrada
-for file in $files; do
-    if ! tar ztf $gz $file &> /dev/null; then #No es troba el fitxer
-        tmpd="src"
-        [[ ! -d $tmpd ]] && mkdir $tmpd
-        tar xf $gz -C $tmpd
-        cp $file $tmpd/$file
-        tar zcf 2.$gz * -C $tmpd
-        # rm -rf $tmpd
-    # else
-    
+filestoadd=()
+tmpdir=$(mktemp -d) # Directorio temporal para la descompresion y posterior compresion 
+
+# Si el comp. existe => selecciono los archivos que quiero añadir
+for file in ${files[@]}; do
+    # Si el archivo existe => miro de que se use -k
+    if tar ztf $gz | grep $file > /dev/null; then
+        if [[ $K -eq 1 ]]; then
+            if [[ $D -eq 1 ]]; then
+                echo "[-D] - Se crearia duplicado con fecha de: $file"
+                echo "[-D] - $file: Archivo marcado para añadir a $gz."
+            else
+                datedfile="$file.$(date +%Y.%m.%d:%H.%M.%S)"
+                cp -r $file $datedfile
+                mv $datedfile $tmpdir
+                filestoadd+=($datedfile)
+            fi
+        else
+            echo "$file existeix a: $gz. Usa -k per a crear una copia amb data."
+        fi
+    else
+        if [[ $D -eq 1 ]]; then
+            echo "[-D] - $file: Archivo marcado para añadir a $gz."
+        else
+            cp -r $file $tmpdir
+            filestoadd+=($file)
+        fi
     fi
-    # Comprova si el fitxer és un directori
-    # if [ -d "$fitxer" ]; then
-    #     if [ $K -eq 1 ]; then
-    #         tar rf "$fitxer_sortida" --transform="s|^\./|$fitxer.$(date +%Y.%m.%d:%H.%M.%S)/|" "$fitxer"
-    #     else
-    #         tar rf "$fitxer_sortida" "$fitxer"
-    #     fi
-    # else
-    #     # Comprova si el fitxer ja existeix a l'arxiu comprimit
-    #     if tar tf "$fitxer_sortida" | grep -q "$fitxer"; then
-    #         if [ $K -eq 1 ]; then
-    #             nom_fitxer=$(basename "$fitxer")
-    #             data_modificacio=$(date -r "$fitxer" +%Y.%m.%d:%H.%M.%S)
-    #             tar rf "$fitxer_sortida" --transform="s|^\./|$nom_fitxer.$data_modificacio/|" "$fitxer"
-    #         else
-    #             echo "El fitxer $fitxer ja existeix a l'arxiu comprimit."
-    #         fi
-    #     else
-    #         tar rf "$fitxer_sortida" "$fitxer"
-    #     fi
-    # fi
 done
 
-# echo "S'ha afegit correctament tots els fitxers a l'arxiu comprimit."
-
+# de los archivos que añadir => los añado 
+[[ $D -eq 1 ]] && echo "[-D] - Se descomprime el contenido de $gz en $tmpdir"
+[[ $D -eq 1 ]] && echo "[-D] - Se comprime el contenido de $tmpdir en $gz ( con los archivos por añadir )"
+if [[ ! -z $filestoadd ]]; then  
+    tar zxf $gz -C $tmpdir
+    tar zcf $gz -C $tmpdir .    
+fi
+rm -rf $tmpdir
